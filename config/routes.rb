e0f71f04180912b262510ffc5b0867e37e370741
lyuxueyue <lyuxueyue@uchicago.edu>
2022-05-27 04:47:21 +0000

    My commit message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "movies", :action => "index" })
   # Routes for the Bookmark resource:
 
   get("/add_bookmark", { :controller => "movies", :action =>"add_user_bookmark"})
